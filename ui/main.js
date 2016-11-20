ccccb59907998ac5ee1e92a6d7dcae2970fb7666
suryatejareddy <gvssuryatejareddy@gmail.com>
2016-11-20 21:26:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
-console.log('Loaded!');
+ var button =document.getElementById('counter');
+ var counter=0;
+ button.onclick=function(){
+     var request=new XMLHttpRequest();
+     counter=counter+1;
+     var span=document.getElementById('count');
+     span.innerHTML=counter.toString();
+ };
 
 
 
