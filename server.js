767969db092b2884c714a2e0c5eb35383f23a01b
suryatejareddy <gvssuryatejareddy@gmail.com>
2016-11-20 20:11:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,7 @@ var articles={
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.js',function(req,res){res.sendFile(path.join(_dirname,'ui','main.js'));});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
