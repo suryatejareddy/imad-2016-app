c69c0049247fe6abaaf8af8c48d6de3e2579b9d2
suryatejareddy <gvssuryatejareddy@gmail.com>
2016-11-22 15:51:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,17 @@
  var button =document.getElementById('counter');
- counter=counter+1;
+ var counter=0;
  
  button.onclick=function(){
-     var request=new XMLHttpRequest();
-     request.onreadystatechange=function(){
-     if(request.readyState===XMLHttpRequest.DONE){
-         if(request.status===200){
-             var counter=request.responseText;
-              var span=document.getElementById('count');
-               span.innerHTML=counter.toString();
+      counter=counter+1;
+     var span=document.getElementById('count');
+     span.innerHTML=counter.toString();
     
-      }
-     }
-     };
-     request.open('GET','suryatejareddy.imad.hasura-app.io/counter',true);
-     request.send(null);
  };
+    
+            
+    
+    
+ 
  
 
 
