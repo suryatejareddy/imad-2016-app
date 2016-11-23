63715332d4014d48bb0e0177f210d6daac83226e
suryatejareddy <gvssuryatejareddy@gmail.com>
2016-11-23 17:00:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,8 +105,9 @@ app.get('/ui/madi.png', function (req, res) {
 
 var names = [];
 
-app.get('/submit-name/:name',function(req,res){
-    var name = req.params.name;
+app.get('/submit-name',function(req,res){
+    
+    var name = req.query.name;
     names.push(name);
     
     res.send(JSON.stringify(names));
