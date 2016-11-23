1ba97b57aa181c7518f4d533d47770e954539ea8
suryatejareddy <gvssuryatejareddy@gmail.com>
2016-11-23 16:12:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,27 @@
 // counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 
 button.onclick = function () {
     
     //Make a request to the counter endpoint
+    var request = new XMLHttpRequest();
     
     //capture the response and store it in a variable
+    regquest.onreadystatechange = function (){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if (request.status=== 200){
+                var counter = requesst.responseText;
+                 var span = document.getElementById('count');
+                 span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://http://suryatejareddy.imad.hasura-app.io/counter',true);
+    request.send(null);
+   
     
-    //render the variable in the correctbspan
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+   
 };
  
  
