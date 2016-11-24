65d9667c29704d33049e7536cb54eb8e5584739f
suryatejareddy <gvssuryatejareddy@gmail.com>
2016-11-24 15:55:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick = function () {
    
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
       var request = new XMLHttpRequest();
@@ -47,6 +46,8 @@ submit.onclick = function () {
             }
         }
     };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://http://suryatejareddy.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
    
