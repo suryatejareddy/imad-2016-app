30eb98606645d0684dca7c9f4b1454712623e74d
suryatejareddy <gvssuryatejareddy@gmail.com>
2016-11-25 12:25:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,20 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-<form method='post'>
-  NAME: <input type='text' name='name' id='name' /><br />
 
-  Email: <input type='text' name='email' id='email' /><br />
-
-  Website: <input type='text' name='website' id='website' /><br />
-
-  Comment:<br />
-  <textarea name='comment' id='comment'></textarea><br />
-
-  <input type='hidden' name='articleid' id='articleid' value='<? echo $_GET["id"]; ?>' />
-
-  <input type='submit' value='Submit' />  
-</form>
 
 
 var articles={
