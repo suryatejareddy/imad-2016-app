0163bc131e17ba28a2aa291ac499fefbf7bb67c3
suryatejareddy <gvssuryatejareddy@gmail.com>
2016-11-27 10:42:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,14 +21,7 @@ var articles={
              this is my first web app and i am very happy to share.
              
          </p>
-         <h2>movies</h2>
-         <img src="2wCEAAkGBxMTEhUTExMVFhUXGRgYGBgXGBoYGxgYGxgdGhgXHhgaHSggGh0lHRkYITEiJSkrLi4uFx8zODMtNygtLisBCgoKDg0OGxAQGy0lICYvLS8tLS0tLS0tLS8tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLf" alt="W3Schools.com">
-<  style="width:304px;height:228px;">
-  <!-- begin wwww.htmlcommentbox.com -->
- <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Comment Form</a> is loading comments...</div>
- <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
- <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&opts=16862&num=10&ts=1480096413156");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
-<!-- end www.htmlcommentbox.com -->
+        
           `
  },
   articleTwo: {
