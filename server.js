a354c49240f45504681e7e3cb3df15b1e5887b51
suryatejareddy <gvssuryatejareddy@gmail.com>
2016-11-27 15:54:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var articles={
           heading:'electronics',
           date:'sep16,2016',
      content:`
-     <body background="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlQcQ2C08AgOfSeJp9AHJ2Mm3seE5sWoX0tPdBLLoEZSUgRIEM">
+     <body background="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqYzvtS8tUmO9zMG2uhB1vB8WXW6yq5EBpKe2wsPJ5lWWYQiW4">
      <p>
              this is my first web app and i am very happy to share.
              
